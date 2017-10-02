1d730c4c1ab544569219763c89634601aa3f625a
jDeerin-hackGT <2016deerinjo@gmail.com>
2017-10-02 19:39:12 -0400

    Model.java now stores Users with a HashMap

--- Rat_Tracking_Application/app/src/main/java/area52/rat_tracking_application/model/Model.java
+++ Rat_Tracking_Application/app/src/main/java/area52/rat_tracking_application/model/Model.java
@@ -4,6 +4,9 @@ package area52.rat_tracking_application.model;
  * Created by Eric on 9/25/2017.
  */
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * Created by robert waters on 1/5/17.
  *
@@ -28,7 +31,7 @@ public class Model {
     /**
      * holds the list of all Users
      */
-    private User[] _users;
+    private HashMap<String, User> _users;
 
     /**
      * the currently logged in user
@@ -39,7 +42,7 @@ public class Model {
      * make a new model
      */
     private Model() {
-        _users = new User[5];
+        _users = new HashMap<>();
         loadTestData();
     }
 
@@ -47,11 +50,11 @@ public class Model {
      * populate model with data to test app.
      */
     public void loadTestData() {
-        _users[0] = new User("Eric", "eric", "numbers@1");
-        _users[1] = new User("Jake", "jake", "numbers@2");
-        _users[2] = new User("Heejoo", "heejoo", "numbers@3");
-        _users[3] = new User("Grace", "grace", "numbers@4");
-        _users[4] = new User("Randall", "randy", "numbers@5");
+        _users.put("eric", new User("Eric", "eric", "numbers@1"));
+        _users.put("jake", new User("Jake", "jake", "numbers@2"));
+        _users.put("heejoo", new User("Heejoo", "heejoo", "numbers@3"));
+        _users.put("grace", new User("Grace", "grace", "numbers@4"));
+        _users.put("randy", new User("Randall", "randy", "numbers@5"));
     }
 
     /**
@@ -60,7 +63,7 @@ public class Model {
      * @return a list of the users registered to use the app
      */
     public User[] getUsers() {
-        return _users;
+        return _users.values().toArray(new User[_users.size()]);
     }
 
     /**
