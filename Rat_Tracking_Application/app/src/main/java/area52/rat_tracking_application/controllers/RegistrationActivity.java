749cb3a9ab4fc9c64965d019c71ae368a49ee21f
jDeerin-hackGT <2016deerinjo@gmail.com>
2017-10-02 22:34:23 -0400

    Implemented user registration (email/isAdmin currently not used); fixed typos in activity_registration.xml

--- Rat_Tracking_Application/app/src/main/java/area52/rat_tracking_application/controllers/RegistrationActivity.java
+++ Rat_Tracking_Application/app/src/main/java/area52/rat_tracking_application/controllers/RegistrationActivity.java
@@ -1,17 +1,25 @@
 package area52.rat_tracking_application.controllers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 
 import area52.rat_tracking_application.R;
+import area52.rat_tracking_application.model.Model;
+import area52.rat_tracking_application.model.User;
 
 public class RegistrationActivity extends AppCompatActivity {
     EditText usernameText;
     EditText emailText;
     EditText passwordText;
     CheckBox adminCheckbox;
+
+    Button registerButton;
+    Button cancelButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +30,26 @@ public class RegistrationActivity extends AppCompatActivity {
         emailText = (EditText) findViewById(R.id.editText_email);
         passwordText = (EditText) findViewById(R.id.editText_password);
         adminCheckbox = (CheckBox) findViewById(R.id.checkBox_admin);
+
+        registerButton = (Button) findViewById(R.id.button_register);
+        cancelButton = (Button) findViewById(R.id.button_cancel);
+    }
+
+    public void registerUser(View view) {
+        String username = usernameText.getText().toString();
+        String email = emailText.getText().toString();
+        String password = passwordText.getText().toString();
+        boolean isAdmin = adminCheckbox.isChecked();
+
+        if (username != null && email != null && password != null) {
+            User newlyRegisteredUser = new User(username, username, password);
+            Model.getInstance().addUser(newlyRegisteredUser);
+            goBackToLoginScreen(view);
+        }
+    }
+
+    public void goBackToLoginScreen(View view) {
+        Intent loginScreenActivity = new Intent(this, MainActivity.class);
+        startActivity(loginScreenActivity);
     }
 }
