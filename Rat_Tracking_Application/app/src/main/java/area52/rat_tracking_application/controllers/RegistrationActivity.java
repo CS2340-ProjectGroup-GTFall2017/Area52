d744d470cf70f7a5f29abd08ceb4b217c34168d9
jDeerin-hackGT <2016deerinjo@gmail.com>
2017-10-02 22:09:26 -0400

    Added EditText view references to RegistrationActivity

--- Rat_Tracking_Application/app/src/main/java/area52/rat_tracking_application/controllers/RegistrationActivity.java
+++ Rat_Tracking_Application/app/src/main/java/area52/rat_tracking_application/controllers/RegistrationActivity.java
@@ -1,25 +1,26 @@
 package area52.rat_tracking_application.controllers;
 
-import android.annotation.SuppressLint;
-import android.content.Context;
-import android.content.Intent;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
-import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 import area52.rat_tracking_application.R;
 
 public class RegistrationActivity extends AppCompatActivity {
+    EditText usernameText;
+    EditText emailText;
+    EditText passwordText;
+    CheckBox adminCheckbox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        usernameText = (EditText) findViewById(R.id.editText_username);
+        emailText = (EditText) findViewById(R.id.editText_email);
+        passwordText = (EditText) findViewById(R.id.editText_password);
+        adminCheckbox = (CheckBox) findViewById(R.id.checkBox_admin);
     }
 }
