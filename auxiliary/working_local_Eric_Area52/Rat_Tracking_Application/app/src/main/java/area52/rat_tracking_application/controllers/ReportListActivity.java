3868293f9962a713faccaca578db3340934b61ae
ERIC RICHARD MALAVENDA <ERICMALAVENDA@GATECH.EDU>
2017-10-26 22:03:01 -0400

    ...

---
+++ auxiliary/working_local_Eric_Area52/Rat_Tracking_Application/app/src/main/java/area52/rat_tracking_application/controllers/ReportListActivity.java
@@ -0,0 +1,8 @@
+package area52.rat_tracking_application.controllers;
+
+/**
+ * Created by Eric on 10/26/2017.
+ */
+
+public class ReportListActivity {
+}
---
+++ auxiliary/working_local_Eric_Area52/Rat_Tracking_Application/app/src/main/java/area52/rat_tracking_application/model/Borough.java
@@ -0,0 +1,8 @@
+package area52.rat_tracking_application.model;
+
+/**
+ * Created by Eric on 10/26/2017.
+ */
+
+public enum Borough {
+}
