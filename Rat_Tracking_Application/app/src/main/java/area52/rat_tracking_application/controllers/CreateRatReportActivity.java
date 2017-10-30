59ee163facf0c48410b739284a62c3f2f3888923
jDeerin-hackGT <2016deerinjo@gmail.com>
2017-10-30 10:55:45 -0400

    Implemented Rat Report adding functionality; no validation, error-prone

--- Rat_Tracking_Application/app/src/main/java/area52/rat_tracking_application/controllers/CreateRatReportActivity.java
+++ Rat_Tracking_Application/app/src/main/java/area52/rat_tracking_application/controllers/CreateRatReportActivity.java
@@ -2,14 +2,22 @@ package area52.rat_tracking_application.controllers;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Debug;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+import java.util.Date;
+import java.util.Random;
+
 import area52.rat_tracking_application.R;
+import area52.rat_tracking_application.model.RatReport;
+import area52.rat_tracking_application.model.RatReportLoader;
+import area52.rat_tracking_application.model.ReportLocation;
 
 public class CreateRatReportActivity extends AppCompatActivity {
     private Button cancelButton;
@@ -28,6 +36,22 @@ public class CreateRatReportActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_rat_report);
 
+        getRatReportFieldViews();
+        setUpButtonClickListeners();
+    }
+
+    private void getRatReportFieldViews() {
+        locationType = (Spinner) findViewById(R.id.spinner_locationType);
+        borough = (Spinner) findViewById(R.id.spinner_borough);
+
+        latitude = (EditText) findViewById(R.id.editText_lat);
+        longitude = (EditText) findViewById(R.id.editText_longitude);
+        city = (EditText) findViewById(R.id.editText_City);
+        address  = (EditText) findViewById(R.id.editText_address);
+        zipCode = (EditText) findViewById(R.id.editText_zipcode);
+    }
+
+    private void setUpButtonClickListeners() {
         cancelButton = (Button) findViewById(R.id.button_cancel);
         cancelButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,7 +74,26 @@ public class CreateRatReportActivity extends AppCompatActivity {
         context.startActivity(intent);
     }
 
-    private void createNewRatReport(View view) {
+    private void createNewRatReport(View view) { //TODO: Refactor this mess
+        String locType = locationType.getSelectedItem().toString();
+        String bor = borough.getSelectedItem().toString();
+
+        String addr = address.getText().toString();
+        String cit = city.getText().toString();
+
+        double lat = Double.valueOf(latitude.getText().toString());
+        double lon = Double.valueOf(longitude.getText().toString());
+        int zip = Integer.valueOf(zipCode.getText().toString());
+
+        Random rand = new Random();
+        long key = rand.nextLong();
+        while (RatReportLoader.reports.containsKey(key)) { //TODO: Come up with a safer, better way to get a unique key
+            key = rand.nextLong();
+        }
+
+        ReportLocation reportLoc = new ReportLocation(lat, lon, locType, addr, cit, bor, zip);
+        RatReport newReport = new RatReport(key, new Date(), reportLoc); //TODO: Relies on Date constructor defaulting to current time
+        RatReportLoader.reports.put(key, newReport);
 
         returnToRatReportList(view);
     }
--- Rat_Tracking_Application/app/src/main/java/area52/rat_tracking_application/controllers/ReportListActivity.java
+++ Rat_Tracking_Application/app/src/main/java/area52/rat_tracking_application/controllers/ReportListActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -52,6 +53,8 @@ public class ReportListActivity extends AppCompatActivity {
 
         reportAdapter = new RatReportAdapter(this);
         ratReportRecyclerView.setAdapter(reportAdapter);
+
+        Log.d("Debug", "# rat reports: " + RatReportLoader.reports.keySet().size());
     }
 }
 
